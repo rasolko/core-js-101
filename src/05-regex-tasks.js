4851da7a6485dc5a063c9393b2aa1969198bd317
rasolko <ivan.rasolko1@gmail.com>
2020-12-14 22:21:44 +0300

    compelte tasks

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /{([0-9A-Ga-g]){8}-([0-9A-Ga-g]){4}-([0-9A-Ga-g]){4}-([0-9A-Ga-g]){4}-([0-9A-Ga-g]){12}}/;
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /.*p.t.*/;
 }
 
 
@@ -78,8 +78,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const reg = `(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])^[a-zA-Z0-9]{${minLength},}$`;
+  return new RegExp(reg);
 }
 
 
